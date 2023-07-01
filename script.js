1235fadcfa7157d13e364177153e0918b3c94457
Hemant kumar <116268777+hemant0933@users.noreply.github.com>
2023-07-01 22:54:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here!
 
+let list = document.getElementById("infi-list");
+let items = ["List Item 11", "List Item 12"];
+
+function addItems() {
+  for (let i = 0; i < items.length; i++) {
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(items[i]));
+    list.appendChild(li);
+  }
+}
+
+addItems();
+
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >=
+    document.body.offsetHeight - 100
+  ) {
+    addItems();
+  }
+});
